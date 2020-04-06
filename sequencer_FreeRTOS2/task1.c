25a4b06a70f49a4da8b16308666869010610356a
Roberto Baquerizo <roba8460@colorado.edu>
2020-04-05 19:49:54 -0600

    cleanup

--- sequencer_FreeRTOS2/task1.c
+++ sequencer_FreeRTOS2/task1.c
@@ -34,24 +34,6 @@ extern bool abortS1;
 static TaskHandle_t taskHandle;
 static void taskOne( void *pvParameters )
 {
-//   struct timeval current_time_val;
-//   double current_time;
-//   unsigned long long S1Cnt=0;
-//   threadParams_t *threadParams = (threadParams_t *)threadp;
-//
-//   gettimeofday(&current_time_val, (struct timezone *)0);
-//   syslog(LOG_CRIT, "Frame Sampler thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
-//   printf("Frame Sampler thread @ sec=%d, msec=%d\n", (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
-//
-//   while(!abortS1)
-//   {
-//       sem_wait(&semS1);
-//       S1Cnt++;
-//
-//       gettimeofday(&current_time_val, (struct timezone *)0);
-//       syslog(LOG_CRIT, "Frame Sampler release %llu @ sec=%d, msec=%d\n", S1Cnt, (int)(current_time_val.tv_sec-start_time_val.tv_sec), (int)current_time_val.tv_usec/USEC_PER_MSEC);
-//   }
-//
    portTickType wakeTick = 0;
    portTickType doneTick = 0;
    portTickType wcet = 0;
